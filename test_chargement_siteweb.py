9ff0476821c053fb6f223ebe2cadb53b229dca0a
chaabeni slah edin <47463609+slahedin@users.noreply.github.com>
2025-05-16 14:39:36 +0100

    Update test_chargement_siteweb.py

--- test_chargement_siteweb.py
+++ test_chargement_siteweb.py
@@ -6,7 +6,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import HtmlTestRunner  # Nouveau module importé
+import HtmlTestRunner
+
 
 
 class TestSibtelMenu(unittest.TestCase):
