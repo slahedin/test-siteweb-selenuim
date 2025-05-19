d30e4ad2bfc8139bfd913f8ff9eaa377a3c236ab
chaabeni slah edin <47463609+slahedin@users.noreply.github.com>
2025-05-19 11:27:44 +0100

    Update watcher.py

--- watcher.py
+++ watcher.py
@@ -2,18 +2,20 @@ import os
 import glob
 import subprocess
 
-# Dossier où chercher les fichiers .txt
-folder_path = r"C:\Users\Admin\Desktop\selenuim_tests\test"
+# Dossier contenant les fichiers .txt
+folder_path = r"C:\\Users\\Admin\\Desktop\\selenuim_tests\\test"
 
-# Rechercher les fichiers .txt
+# Recherche de fichiers .txt
 txt_files = glob.glob(os.path.join(folder_path, "*.txt"))
 
 if txt_files:
     print(f"[INFO] Fichier détecté : {txt_files[0]}")
     print("[INFO] Lancement du test Selenium...")
 
-    # Lancer le script principal (attention au chemin selon Jenkins)
-    subprocess.run(["python", "C:\\Users\\Admin\\Desktop\\selenuim_tests\\test\\test_chargement_siteweb.py"])
-
+    # Exécution du script de test via unittest directement
+    subprocess.run([
+        "python", "-m", "unittest",
+        "test_chargement_siteweb.py"
+    ])
 else:
     print("[INFO] Aucun fichier .txt trouvé.")
